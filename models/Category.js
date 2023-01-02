f019f3dbd090ecef177da915c285529e95da50ee
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-01-02 22:23:41 +0000

    added category.js items

--- models/Category.js
+++ models/Category.js
@@ -1,19 +1,35 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Category extends Model {}
 
 Category.init(
-  {
-    // define columns
+  {// new column named "id"
+    id: {
+      // data type must be a number
+      type: DataTypes.INTEGER,
+      // needs to be included
+      allowNull: false,
+      // allocates this a primary key
+      primaryKey: true,
+      // keeps auto adding one each time
+      autoIncrement: true,
+    },
+    // new column named "category_name"
+    category_name: {
+      // data type is a string
+      type: DataTypes.STRING,
+      // needs to be included
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
 
