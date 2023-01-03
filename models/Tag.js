a026b5a5b2bd07f2c8c15b5bdff67cdb674dbea6
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-01-03 21:18:28 +0000

    completed tag table

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,34 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // new column named id
+    id: {
+      // data type needs to be a number
+      type: DataTypes.INTEGER,
+      // needs to be included
+      allowNull: false,
+      // add the primary key to this value
+      primaryKey: true,
+      // keep adding on one after each additional tag
+      autoIncrement: true,
+    },
+    // new column named tag_name
+    tag_name: {
+      // data type needs to be a string
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
