d8476ee83e041fdaf35a18ea0d955253a0674caa
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-01-03 23:01:44 +0000

    completed relationships

--- models/index.js
+++ models/index.js
@@ -1,16 +1,36 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  // // When we delete a product, make sure to also delete the associated data
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // makes it go through the ProductTag junction table
+  through: ProductTag,
+  // as: 'product_Tags'
+  foreignKey: "product_id",
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // makes it go through the ProductTag junction table
+  through: ProductTag,
+  // as: 'product_Tags'
+  foreignKey: "tag_id",
+});
 
 module.exports = {
   Product,
