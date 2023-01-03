d37ecd61f6144798210068e1041da6ae675e49d4
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-01-03 21:13:18 +0000

    complete product table

--- models/Product.js
+++ models/Product.js
@@ -8,8 +8,58 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
+  {// new column named "id"
+    id: {
+      // data type must be a number
+      type: DataTypes.INTEGER,
+      // needs to be included
+      allowNull: false,
+      // allocates this a primary key
+      primaryKey: true,
+      // keeps auto adding one each time
+      autoIncrement: true,
+    },
+    // new column named "product_name"
+    product_name: {
+      // data type must be a string
+      type: DataTypes.STRING,
+      // needs to be included
+      allowNull: false,
+    },
+    // new column to show the price 
+    price: {
+      // data type must be a decimal
+      type: DataTypes.DECIMAL,
+      // needs to be included
+      allowNull: false,
+      // needs to make sure that a decimal is present
+      validate: {
+        isDecimal: true,
+      }
+    },
+    // new column to show the amount of stock
+    stock: {
+      // data type must be a number
+      type: DataTypes.INTEGER,
+      // needs to be included
+      allowNull: false,
+      // start at 10 items
+      defaultValue: 10,
+      // needs to make sure that the amount is a number
+      validate: {
+        isNumeric: true,
+      }
+    },
+    // new column to show the categories id number
+    category_id: {
+      // data type must be a number
+      type: DataTypes.INTEGER,
+      // make sure to show the products category id number also
+      references: {
+        model: 'Category',
+        key: 'id',
+      }
+    },
   },
   {
     sequelize,
