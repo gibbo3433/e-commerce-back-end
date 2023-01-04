2600298d884ae9483a44004e1a671d640f0cd0b5
Jordan Gibbs <jgibbs291@hotmail.co.uk>
2023-01-04 19:53:12 +0000

    complete product-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,37 +1,90 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-// This will find all of the products in the entire category ?????
-router.get('/', async (req, res) => {
+// This will find all of the products in the entire category
+router.get("/", async (req, res) => {
   try {
+    // find all in the category
     const categoryData = await Category.findAll({
       include: [{ model: Product }],
+      // response showing the categoryData
     });
     res.status(200).json(categoryData);
+    // catch if there is an error
   } catch (err) {
     res.status(500).json(err);
   }
-  // find all categories
-  // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    // find a product in the in the category by its id value
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    // if no id exists, then send a message saying no category with that id!
+    if (!categoryData) {
+      res.status(404).json({ message: "No category with that id!" });
+      return;
+    }
+    // response showing the categoryData
+    res.status(200).json(categoryData);
+
+    // catch if there is an error
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", async (req, res) => {
+  try {
+    // create a new category and require a body
+    const categoryData = await Category.create(req.body);
+
+    // response showing the categoryData
+    res.status(200).json(categoryData);
+
+    // catch if there is an error
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+// update a category by its id value
+router.put("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.update(req.body, {
+      // shows which id needs to be updated or changed
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(categoryData);
+
+    // catch if there is an error
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+// delete a category by its id value
+router.delete("/:id", async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      // shows which id needs to be updated or changed
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(categoryData);
+
+    // catch if there is an error
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
